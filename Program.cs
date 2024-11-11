cd91349b08dca56a1c8cbaa35571a4a2284faca6
fthann <fathanalfrq@gmail.com>
2024-11-11 14:30:19 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+            // Method static
+            static void methodStatic() {
+                Console.WriteLine("Saya adalah method static");
+            }
+
+            // Method non-static
+            void nonStaticMethod() {
+                Console.WriteLine("Saya adalah method NON - static");
+            }
+
+            
+
+            // Method entry point (Method Main)
+            static void Main(string[] args) {
+                // pemanggil method static
+                methodStatic();
+
+                // pemanggil method non staticx
+                Program program = new Program();
+                program.nonStaticMethod();
+            } 
+
+
+
+            // Method dengan return
+            static string fullName(string namaSaya) {
+                return namaSaya;
+            }
+
+            // Method tanpa return
+            static void nimName(string namaSaya) {
+                Console.WriteLine(nimName);
+            }
+
+            // Method entry point (Method Main)
+            static void main(string[] args) {
+                fullName("pabaji");
+                Console.WriteLine(fullName("fortinaiti"));
+                nimName("babyo");
+            }
+    }
+}
